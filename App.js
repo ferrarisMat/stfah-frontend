0f7f008b7f2f6784d4fe7c585d2313e9e92d889b
lauweddu <ldurieux97@gmail.com>
2020-04-04 15:17:45 +0200

    feature chart

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { StyleSheet, Text, View, Platform } from "react-native";
 import Pedometer from "./components/Pedometer/Pedometer";
+import Chart from "./components/Chart";
 
 
 function App(){
   return (
-    <Pedometer />
+    <View>
+      <Pedometer />
+      <Chart />
+    </View>
   )
 };
 
