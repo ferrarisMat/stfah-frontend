317a0bb186653473c819caf557b498ce6702ecd9
lauweddu <ldurieux97@gmail.com>
2020-04-04 22:50:43 +0200

    Chart library changed

--- components/Chart.js
+++ components/Chart.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Dimensions } from "react-native";
-import { LineChart } from "react-native-chart-kit";
+import { Dimensions, View, StyleSheet } from "react-native";
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
 import Actions from "../data/actions";
 import UserData from "../data/userdata";
 
@@ -14,7 +15,8 @@ function formatDate(date){
     return date
  }
 
-export default function Chart() {
+ export default class Chart extends React.PureComponent {
+    render() {
     // Initiate the array with total points per day
     let days = [];
     let TotalPointsPerDay = {};
@@ -48,47 +50,36 @@ export default function Chart() {
     });
 
     // Data for the chart
-    const data = {
-        labels: days,
-        datasets: [
-            {
-                data: [
-                    TotalPointsPerDay[days[0]],
-                    TotalPointsPerDay[days[1]],
-                    TotalPointsPerDay[days[2]],
-                    TotalPointsPerDay[days[3]],
-                    TotalPointsPerDay[days[4]],
-                    TotalPointsPerDay[days[5]],
-                    TotalPointsPerDay[days[6]]
-                ]
-            }
-        ]
-    }
-    // Configuration object for the chart
-    const chartConfig = {
-        backgroundColor: "rgba(0,0,0,0)",
-        decimalPlaces: 2, // optional, defaults to 2dp
-        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-        propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#FA9F42"
-        }
-    }
-    const chartStyle = {
-        marginVertical: 8,
-    }
+    const dataArea = [
+        TotalPointsPerDay[days[0]],
+        TotalPointsPerDay[days[1]],
+        TotalPointsPerDay[days[2]],
+        TotalPointsPerDay[days[3]],
+        TotalPointsPerDay[days[4]],
+        TotalPointsPerDay[days[5]],
+        TotalPointsPerDay[days[6]]
+    ]
 
     return(
-        <LineChart
-            data={data}
-            width={Dimensions.get("window").width} // from react-native
-            height={220}
-            fromZero={true}
-            yAxisInterval={1} // optional, defaults to 1
-            chartConfig={chartConfig}
-            style={chartStyle}
-        />
+        <AreaChart
+                style={{ height: 200 }}
+                data={dataArea}
+                animate={true}
+                contentInset={{ top: 30, bottom: 30 }}
+                curve={shape.curveNatural}
+                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+        >
+                <Grid />
+        </AreaChart>
     )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      borderTopWidth: 0.5,
+      borderTopColor: 'rgba(0, 0, 0, .7)',
+      borderRightWidth: 0.5,
+      borderRightColor: 'rgba(0, 0, 0, .7)'
+    }
+  });
