4b334bf1837949ef27fd02431f8e3de0489741d4
Mamboleoo <mambolouis@gmail.com>
2020-04-05 00:39:23 +0200

    Load fonts

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,29 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text, AsyncStorage } from "react-native";
 import Pedometer from "../components/Pedometer/Pedometer";
 import Navigation from "../components/Navigation";
 import Chart from "../components/Chart";
 import SafeAreaView from 'react-native-safe-area-view';
+import { Objects } from '../styles/objects';
 
 export default function Home() {
+  const [userName, updateUserName] = useState("");
+
+  useEffect(() => { getNameFromStorage() }, []);
+
+  const getNameFromStorage = async () => {
+    try {
+      const value = await AsyncStorage.getItem("name");
+      updateUserName(value);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{ flex: 1 }}>
+        <Text style={Objects.title}>{userName}</Text>
         <Pedometer />
         <Chart />
       </View>
---
+++ styles/objects.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export let Objects = StyleSheet.create({
+  title: {
+  	fontSize: 40,
+  	color: 'red',
+  	fontFamily: 'Rosario Light'
+  }
+});
