105f6b68a65f602771c795a0d9c57603a0647862
lauweddu <ldurieux97@gmail.com>
2020-04-04 23:55:21 +0200

    Added features to the chart

--- components/Chart.js
+++ components/Chart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Dimensions, View, StyleSheet } from "react-native";
-import { AreaChart, Grid } from 'react-native-svg-charts'
+import { LineChart, Grid, YAxis, XAxis } from 'react-native-svg-charts';
+import { Circle, Path, Defs, LinearGradient, Stop, G, Line, Rect, Text } from 'react-native-svg';
 import * as shape from 'd3-shape'
 import Actions from "../data/actions";
 import UserData from "../data/userdata";
@@ -17,61 +18,123 @@ function formatDate(date){
 
  export default class Chart extends React.PureComponent {
     render() {
-    // Initiate the array with total points per day
-    let days = [];
-    let TotalPointsPerDay = {};
-    let lastDay; // Last day of the week
-    for (var i=0; i<7; i++) {
-        var d = new Date();
-        d.setDate(d.getDate() - i);
-        TotalPointsPerDay[formatDate(d)] = 0;
-        days.push(formatDate(d));
+        // Initiate the array with total points per day
+        let totalPointsPerDay = [];
+        let lastDay; // Last day of the week
+        for (var i=0; i<7; i++) {
+            var d = new Date();
+            d.setDate(d.getDate() - i);
+            totalPointsPerDay[formatDate(d)] = 0;
 
-        if(i === 6) {
-            lastDay = formatDate(d);
-        }
-    }
-    days.reverse();
-
-    // Data from the user
-    UserData.forEach((user) => {
-        const today = new Date(user.date);
-        const day = formatDate(today);
-        // If the day is in the interval of the 7 last days
-        if((today.getDate() < lastDay.substring(0,2) && today.getMonth() == lastDay.substring(3,5)) || (today.getMonth() < lastDay.substring(3,5))) {
-            // If the value of the action is a boolean
-            if(Actions[user.action].type === "boolean" && user.value === true) {
-                TotalPointsPerDay[day] += Actions[user.action].scorePerUnit;
-            }
-            else if(Actions[user.action].type === "number" && parseInt(user.value)) {
-                TotalPointsPerDay[day] += Actions[user.action].scorePerUnit * user.value;
+            if(i === 6) {
+                lastDay = formatDate(d);
             }
         }
-    });
 
-    // Data for the chart
-    const dataArea = [
-        TotalPointsPerDay[days[0]],
-        TotalPointsPerDay[days[1]],
-        TotalPointsPerDay[days[2]],
-        TotalPointsPerDay[days[3]],
-        TotalPointsPerDay[days[4]],
-        TotalPointsPerDay[days[5]],
-        TotalPointsPerDay[days[6]]
-    ]
+        // Data from the user
+        UserData.forEach((user) => {
+            const today = new Date(user.date);
+            const day = formatDate(today);
+            // If the day is in the interval of the 7 last days
+            if((today.getDate() < lastDay.substring(0,2) && today.getMonth() == lastDay.substring(3,5)) || (today.getMonth() < lastDay.substring(3,5))) {
+                // If the value of the action is a boolean
+                if(Actions[user.action].type === "boolean" && user.value === true) {
+                    totalPointsPerDay[day] += Actions[user.action].scorePerUnit;
+                }
+                else if(Actions[user.action].type === "number" && parseInt(user.value)) {
+                    totalPointsPerDay[day] += Actions[user.action].scorePerUnit * user.value;
+                }
+            }
+        });
 
-    return(
-        <AreaChart
-                style={{ height: 200 }}
-                data={dataArea}
-                animate={true}
-                contentInset={{ top: 30, bottom: 30 }}
-                curve={shape.curveNatural}
-                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
-        >
-                <Grid />
-        </AreaChart>
-    )
+        // Formatting the array
+        const dataArea = [];
+        for(const key in totalPointsPerDay) {
+            dataArea.push({
+                value: totalPointsPerDay[key],
+                label: key
+            })
+        }
+        dataArea.reverse();
+
+        // Data for the chart
+        const contentInset = { top: 10, bottom: 10 }
+
+        const Decorator = ({ x, y, data }) => {
+            return data.map((value, index) => (
+                <Circle
+                    key={ index }
+                    cx={ x(index) }
+                    cy={ y(value.value) }
+                    r={ 4 }
+                    stroke={ 'rgb(134, 65, 244)' }
+                    fill={ 'white' }
+                />
+            ))
+        }
+
+        const axesSvg = { fontSize: 10, fill: 'grey' };
+        const xAxisHeight = 30
+
+        const Gradient = () => (
+            <Defs key={'gradient'}>
+                <LinearGradient id={'gradient'} x1={'0'} y={'0%'} x2={'100%'} y2={'0%'}>
+                    <Stop offset={'0%'} stopColor={'rgb(134, 65, 244)'}/>
+                    <Stop offset={'100%'} stopColor={'rgb(66, 194, 244)'}/>
+                </LinearGradient>
+            </Defs>
+        )
+
+        /**
+         * Both below functions should preferably be their own React Components
+         */
+
+        const HorizontalLine = (({ y }) => (
+            <Line
+                key={ 'zero-axis' }
+                x1={ '0%' }
+                x2={ '100%' }
+                y1={ y(0) }
+                y2={ y(0) }
+                stroke={ 'grey' }
+                strokeDasharray={ [ 4, 8 ] }
+                strokeWidth={ 2 }
+            />
+        ))
+
+        return(
+            <View style={{ height: 300, flexDirection: 'row' }}>
+                    <YAxis
+                        data={dataArea}
+                        style={{ marginBottom: xAxisHeight }}
+                        contentInset={contentInset}
+                        svg={axesSvg}
+                    />
+                    <View style={{ flex: 1 }}>
+                        <LineChart
+                            data={dataArea}
+                            contentInset={contentInset}
+                            style={{ flex: 1, height: 200 }}
+                            svg={{ stroke: 'url(#gradient)' }}
+                            yAccessor={({ item }) => item.value}
+                        >
+                            <Grid />
+                            <Decorator/>
+                            <Gradient/>
+                            <HorizontalLine/>
+                        </LineChart>
+                        <XAxis
+                            style={{ height: xAxisHeight }}
+                            data={dataArea}
+                            formatLabel={(value) => value}
+                            contentInset={{ contentInset }}
+                            svg={axesSvg}
+                            yAccessor={({ index }) => index}
+                            formatLabel={(_, index) => dataArea[ index ].label}
+                        />
+                    </View>
+                </View>
+        )
     }
 }
 
