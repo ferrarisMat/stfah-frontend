fd0512305415c28cbe685078a7bcc93e8225eac5
Maël Brunet <maelbrunet@gmail.com>
2020-04-05 12:54:18 +0200

    Style pedometer

--- pages/Home.js
+++ pages/Home.js
@@ -31,8 +31,10 @@ export default function Home({ navigation }) {
         <Text style={Styles.title}>{userName}</Text>
         <Text style={{marginTop: 10}}>Total</Text>
         <View style={Styles.textRow}><Text style={[Styles.redTitle, {marginRight: 7}]}>-60</Text><Text style={Styles.body}>pts</Text></View>
-        <Pedometer />
         <Chart />
+      </SafeAreaView>
+      <SafeAreaView style={Styles.container}>
+        <Pedometer />
       </SafeAreaView>
       <SafeAreaView style={{flex: 1, backgroundColor: '#F4F4F4'}}>
         <Stats />
